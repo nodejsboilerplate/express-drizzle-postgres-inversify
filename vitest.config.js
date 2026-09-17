3052d7428804ae2e6ab05a4a573abed14755a7c3
gitmahin <nimahin25@gmail.com>
2026-09-17 15:44:32 +0600

    chore: update

--- vitest.config.js
+++ vitest.config.js
@@ -12,27 +12,5 @@ export default defineConfig({
     environment: "node",
     globals: true,
     globalSetup: "./src/tests/setup.ts",
-    // tags: [
-    //   {
-    //     name: "apis/user",
-    //     description: "Tests written for user apis.",
-    //   },
-    //   {
-    //     name: "apis/auth",
-    //     description: "Tests written for user apis.",
-    //   },
-    //   {
-    //     name: "services/user",
-    //     description: "Tests written for user services.",
-    //   },
-    //   {
-    //     name: "services/auth",
-    //     description: "Tests written for auth services.",
-    //   },
-    //   {
-    //     name: "services/google",
-    //     description: "Tests written for auth services.",
-    //   },
-    // ],
   },
 });
